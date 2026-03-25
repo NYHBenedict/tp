f43886450e16664c37a4a3badc22c01674a1c994
Yi Qian <taan.yi.qian@gmail.com>
2026-03-25 17:44:48 +0800

    Fix: CI add abstract method

--- src/test/java/seedu/address/logic/commands/ListGradesCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListGradesCommandTest.java
@@ -347,5 +347,10 @@ public class ListGradesCommandTest {
         public DisplayMode getDisplayMode() {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public void updateFilteredGradeList(Predicate<Grade> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 }
