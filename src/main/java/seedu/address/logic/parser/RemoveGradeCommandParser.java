4f4f7f13c9a3f5b6c021e53fca95ef3ad0301a7a
Yi Qian <taan.yi.qian@gmail.com>
2026-03-31 12:42:48 +0800

    update: remove grades via student id

--- src/main/java/seedu/address/logic/parser/RemoveGradeCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemoveGradeCommandParser.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ASSESSMENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_COURSE_CODE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENT_ID;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.RemoveGradeCommand;
@@ -15,28 +15,27 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class RemoveGradeCommandParser implements Parser<RemoveGradeCommand> {
 
-    @Override
-    public RemoveGradeCommand parse(String args) throws ParseException {
+     @Override
+     public RemoveGradeCommand parse(String args) throws ParseException {
         requireNonNull(args);
 
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(
-                args, PREFIX_STUDENT, PREFIX_ASSESSMENT, PREFIX_COURSE_CODE);
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_STUDENT, PREFIX_ASSESSMENT, PREFIX_COURSE_CODE);
+            args, PREFIX_STUDENT_ID, PREFIX_ASSESSMENT, PREFIX_COURSE_CODE);
 
         if (!ParserUtil.arePrefixesPresent(argMultimap,
-                PREFIX_STUDENT, PREFIX_ASSESSMENT, PREFIX_COURSE_CODE)
-                || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    RemoveGradeCommand.MESSAGE_USAGE));
+            PREFIX_STUDENT_ID, PREFIX_ASSESSMENT, PREFIX_COURSE_CODE)
+            || !argMultimap.getPreamble().isEmpty()) {
+                    throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        RemoveGradeCommand.MESSAGE_USAGE));
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(
-                PREFIX_STUDENT, PREFIX_ASSESSMENT, PREFIX_COURSE_CODE);
+       argMultimap.verifyNoDuplicatePrefixesFor(
+            PREFIX_STUDENT_ID, PREFIX_ASSESSMENT, PREFIX_COURSE_CODE);
 
-        Index studentIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_STUDENT).get());
+        String studentId = ParserUtil.parseStudentId(argMultimap.getValue(PREFIX_STUDENT_ID).get());
         Index assessmentIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_ASSESSMENT).get());
         String courseCode = ParserUtil.parseCourseCode(argMultimap.getValue(PREFIX_COURSE_CODE).get());
 
-        return new RemoveGradeCommand(courseCode, studentIndex, assessmentIndex);
+        return new RemoveGradeCommand(courseCode, studentId, assessmentIndex);
     }
 }
